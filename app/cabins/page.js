2fcd88f31c9d6ad73f2f0ef451ebba0a8519888a
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-25 22:29:59 +0400

    implement layout

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,10 +1,6 @@
-import React from "react";
-import Navigation from "../components/Navigation";
-
 const Page = () => {
   return (
     <div>
-      <Navigation />
       <h1>Cabins page</h1>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
 
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>Fotel, Welcome to best hotel in da Country</h1>
       <Link href="/cabins">Explore luxury cabins</Link>
     </div>
