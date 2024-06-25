1995a588fc674a6f546d0531f780328496c7d78e
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-25 23:37:44 +0400

    play with dummy api

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,16 @@
-const Page = () => {
+const Page = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
   return (
     <div>
       <h1>Cabins page</h1>
+
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
