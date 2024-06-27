9d2a226e837f6731baf87b7f25f1368f2d3c9c10
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-27 20:17:53 +0400

    implement and fetch cabins

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,15 @@
 import CabinCard from "../_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
 export const metadata = {
   title: "fotell cabins",
 };
 
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins = await getCabins();
+
+  console.log(cabins);
 
   return (
     <div>
