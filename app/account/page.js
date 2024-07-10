ac2ad7ec5e0e0b06ce7b7111daf6a79dfa9f8793
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-07-10 18:19:47 +0400

    implement middlware for authorazation

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,15 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guests area",
 };
 
-const Page = () => {
+const Page = async () => {
+  const session = await auth();
+  console.log(session);
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Fareedat
+      Welcome, {session?.user?.name}
     </h2>
   );
 };
